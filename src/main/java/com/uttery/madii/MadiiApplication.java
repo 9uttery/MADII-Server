ef908bc1ec02542739eec7fea8c6e4bcbafb1ec2
hojeong2747 <hojeong2747@gmail.com>
2023-11-13 20:04:32 +0900

    프로젝트 생성 (#1)

---
+++ src/main/java/com/uttery/madii/MadiiApplication.java
@@ -0,0 +1,13 @@
+package com.uttery.madii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uttery/madii/MadiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttery.madii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
