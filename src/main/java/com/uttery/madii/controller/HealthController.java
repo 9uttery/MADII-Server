0b0706b0512f6f3d05b3cb2122a8ec2a752f3cdf
hojeong2747 <hojeong2747@gmail.com>
2023-11-13 20:37:46 +0900

    feat: health check용 api 추가 (#2)

---
+++ src/main/java/com/uttery/madii/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.uttery.madii.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
+    }
+}
