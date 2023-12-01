afce908e62403b29f35bf56ec75d33780968ba55
mingeun0507 <tqdlfkg@naver.com>
2023-12-01 16:52:15 +0900

    fix: OpenApiConfig 에서 버전을 주입받지 않도록 수정 (#3)

--- src/main/java/com/guttery/madii/common/config/swagger/OpenApiConfig.java
+++ src/main/java/com/guttery/madii/common/config/swagger/OpenApiConfig.java
@@ -5,29 +5,32 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class OpenApiConfig {
+    private static final String TITLE = "MADII API";
+    private static final String VERSION = "0.0.1"; // API 문서 업데이트 시 버전 수정
+    private static final String DESCRIPTION = "API 명세서";
     private static final String ACCESS_TOKEN = "Access Token";
-
+    private static final String BEARER = "Bearer";
+    private static final String JWT = "JWT";
     @Bean
-    public OpenAPI openAPI(@Value("${springdoc.version}") String springdocVersion) {
+    public OpenAPI openAPI() {
         Info info = new Info()
-                .title("MADII API")
-                .version(springdocVersion)
-                .description("API 명세서");
+                .title(TITLE)
+                .version(VERSION)
+                .description(DESCRIPTION);
 
         SecurityRequirement securityRequirement = new SecurityRequirement()
                 .addList(ACCESS_TOKEN);
 
         SecurityScheme accessTokenSecurityScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
-                .scheme("Bearer")
-                .bearerFormat("JWT")
+                .scheme(BEARER)
+                .bearerFormat(JWT)
                 .in(SecurityScheme.In.HEADER)
                 .name(HttpHeaders.AUTHORIZATION);
 
