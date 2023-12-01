c76c4911b67e07a9c78665cb4893d93d84e4f680
mingeun0507 <tqdlfkg@naver.com>
2023-12-01 15:13:19 +0900

    feat: S3 설정 파일 추가 (#3)

---
+++ src/main/java/com/guttery/madii/common/config/s3/S3Config.java
@@ -0,0 +1,40 @@
+package com.guttery.madii.common.config.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .credentialsProvider(this::getAwsCredentials)
+                .region(Region.of(region))
+                .build();
+    }
+
+    private AwsCredentials getAwsCredentials() {
+        return new AwsCredentials() {
+            @Override
+            public String accessKeyId() {
+                return accessKey;
+            }
+
+            @Override
+            public String secretAccessKey() {
+                return secretKey;
+            }
+        };
+    }
+}
