c87a871af569f0e71cca4d1fd6e1ce861857f40c
mingeun0507 <tqdlfkg@naver.com>
2023-12-04 15:51:26 +0900

    feat: 공통 응답 형식으로 ResponseEntity의 Body에 맵핑하는 ControllerAdvice 정의 (#3)

---
+++ src/main/java/com/guttery/madii/common/advice/ApiResponseBodyAdvice.java
@@ -0,0 +1,31 @@
+package com.guttery.madii.common.advice;
+
+import com.guttery.madii.common.dto.response.ApiResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class ApiResponseBodyAdvice implements ResponseBodyAdvice<Object> {
+    private static final int SUCCESS_STATUS = 200;
+    private static final String SUCCESS_CODE = "OK";
+    private static final String SUCCESS_MESSAGE = "API 요청이 성공했습니다.";
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return MappingJackson2HttpMessageConverter.class.isAssignableFrom(converterType); // String, Boolean 등의 기타 응답에 대해서는 공통 응답 형식 적용 X
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+
+
+
+        return ApiResponse.of(SUCCESS_STATUS, SUCCESS_CODE, SUCCESS_MESSAGE, body); // 200, OK, API 요청이 성공했습니다.
+    }
+}
