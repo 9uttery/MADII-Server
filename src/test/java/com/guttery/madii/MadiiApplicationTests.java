315ca5755da00868cc3f2d21b1b0680c88b4e31f
mingeun0507 <tqdlfkg@naver.com>
2023-12-05 11:16:53 +0900

    test: 간단한 응답 형식 및 에러 처리 테스트 구현 (#3)

--- src/test/java/com/guttery/madii/MadiiApplicationTests.java
+++ src/test/java/com/guttery/madii/MadiiApplicationTests.java
@@ -2,21 +2,18 @@ package com.guttery.madii;
 
 import com.guttery.madii.common.config.security.SecurityConfig;
 import com.guttery.madii.controller.HealthController;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(
@@ -29,7 +26,7 @@ class MadiiApplicationTests {
 
 	@Test
 	@DisplayName("서버 시작 후 Health Check API를 테스트")
-	void contextLoads() throws Exception {
+	void healthCheck() throws Exception {
 		mockMvc.perform(
 				MockMvcRequestBuilders.get("/health")
 						.accept(MediaType.APPLICATION_JSON)
@@ -37,4 +34,33 @@ class MadiiApplicationTests {
 				.andExpect(content().string("healthy"));
 	}
 
+
+	@Test
+	@DisplayName("DTO 반환이 ApiResponseBodyAdvice에서 잘 매핑되는지 테스트")
+	void dtoCheck() throws Exception {
+		mockMvc.perform(
+						MockMvcRequestBuilders.get("/health/dto")
+								.accept(MediaType.APPLICATION_JSON)
+				)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").value(200))
+				.andExpect(jsonPath("$.code").isNotEmpty())
+				.andExpect(jsonPath("$.message").value("API 요청이 성공했습니다."))
+				.andExpect(jsonPath("$.data.status").value("healthy"))
+				.andExpect(jsonPath("$.timestamp", Matchers.matchesPattern("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d+"))); // 정규표현식을 사용하여 timestamp 형식 검증
+	}
+
+	@Test
+	@DisplayName("에러를 throw했을 때 Advice들에 의해 잘 매핑되는지 테스트")
+	void errorCheck() throws Exception {
+		mockMvc.perform(
+						MockMvcRequestBuilders.get("/health/error")
+								.accept(MediaType.APPLICATION_JSON)
+				)
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.status").exists())
+				.andExpect(jsonPath("$.code").exists())
+				.andExpect(jsonPath("$.message").exists())
+				.andExpect(jsonPath("$.timestamp", Matchers.matchesPattern("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d+"))); // 정규표현식을 사용하여 timestamp 형식 검증
+	}
 }
