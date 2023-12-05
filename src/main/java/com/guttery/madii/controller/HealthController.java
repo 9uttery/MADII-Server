cf4a9d84c1d87e5e2273a6cb350a674f6ab0a54a
mingeun0507 <tqdlfkg@naver.com>
2023-12-05 11:17:05 +0900

    feat: 간단한 테스트를 위한 API 구현 (#3)

--- src/main/java/com/guttery/madii/controller/HealthController.java
+++ src/main/java/com/guttery/madii/controller/HealthController.java
@@ -1,5 +1,7 @@
 package com.guttery.madii.controller;
 
+import com.guttery.madii.common.exception.CustomException;
+import com.guttery.madii.common.exception.ErrorDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,18 @@ public class HealthController {
     @GetMapping("/health")
     public String checkHealth() {
         return "healthy";
+    }
+
+    @GetMapping("/health/dto")
+    public HealthResponse checkHealthDto() {
+        return new HealthResponse("healthy");
+    }
+
+    @GetMapping("/health/error")
+    public HealthResponse checkHealthError() {
+        throw CustomException.of(ErrorDetails.BAD_REQUEST);
+    }
+
+    public record HealthResponse(String status) {
     }
 }
