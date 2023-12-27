7998eb868acd562206158a25da320debc627984e
mingeun0507 <tqdlfkg@naver.com>
2023-12-27 12:52:27 +0900

    feat: 기본 QueryDsl Repository 정의 (#6)

---
+++ src/main/java/com/guttery/madii/domain/user/domain/repository/UserQueryDslRepository.java
@@ -0,0 +1,12 @@
+package com.guttery.madii.domain.user.domain.repository;
+
+import com.guttery.madii.domain.user.domain.model.SocialInfo;
+import com.guttery.madii.domain.user.domain.model.User;
+
+import java.util.Optional;
+
+public interface UserQueryDslRepository {
+    Optional<User> findUserByLoginId(String loginId);
+
+    Optional<User> findUserBySocialInfo(SocialInfo socialInfo);
+}
