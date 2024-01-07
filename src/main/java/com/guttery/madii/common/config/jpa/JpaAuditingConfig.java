1c15af297c5781e25a47dabddb20d2e9915ad78c
mingeun0507 <tqdlfkg@naver.com>
2024-01-07 13:53:11 +0900

    feat: JPA Auditing 관련 설정 분리 (#12)

---
+++ src/main/java/com/guttery/madii/common/config/jpa/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.guttery.madii.common.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
