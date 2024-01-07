97291a7669f08293704f9409ee1e0d3391328a41
mingeun0507 <tqdlfkg@naver.com>
2024-01-07 14:01:58 +0900

    fix: 어노테이션 오타 수정 (#12)

--- src/main/java/com/guttery/madii/MadiiApplication.java
+++ src/main/java/com/guttery/madii/MadiiApplication.java
@@ -1,9 +1,9 @@
 package com.guttery.madii;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableJpaAuditing
+@SpringBootApplication
 public class MadiiApplication {
 
 	public static void main(String[] args) {
