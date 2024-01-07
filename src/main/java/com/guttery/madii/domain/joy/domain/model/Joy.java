eb0da6c65a38b60e813bba180a0be993b4a1eaf1
hojeong2747 <hojeong2747@gmail.com>
2024-01-08 01:14:13 +0900

    feat: 소확행(Joy) 도메인 모델 구현 (#15)

---
+++ src/main/java/com/guttery/madii/domain/joy/domain/model/Joy.java
@@ -0,0 +1,42 @@
+package com.guttery.madii.domain.joy.domain.model;
+
+import com.guttery.madii.common.domain.model.BaseTimeEntity;
+import com.guttery.madii.domain.user.domain.model.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "t_joy")
+@Access(AccessType.FIELD)
+public class Joy extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long joyId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator_id")
+    private User user;
+    @Enumerated(EnumType.STRING)
+    private JoyType joyType; // 소확행 타입 (공식, 개인)
+    private Integer joyIconNum; // 소확행 썸네일 번호
+    @Column(length = 30)
+    private String contents; // 소확행 내용
+
+    public Joy(User user, JoyType joyType, Integer joyIconNum, String contents) {
+        this.user = user;
+        this.joyType = joyType;
+        this.joyIconNum = joyIconNum;
+        this.contents = contents;
+    }
+
+    public static Joy createPersonalJoy(User user, Integer joyIconNum, String contents) {
+        return new Joy(user, JoyType.PERSONAL, joyIconNum, contents);
+    }
+
+    public void modifyContents(String contents) {
+        this.contents = contents;
+    }
+}
