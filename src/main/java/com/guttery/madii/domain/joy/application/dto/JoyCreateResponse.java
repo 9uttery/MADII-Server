762333a30882a9f764fbbc0646d9829876ba963e
hojeong2747 <hojeong2747@gmail.com>
2024-01-19 23:41:03 +0900

    chore: 소확행 기록 DTO 불필요한 import 제거 (#18)

--- src/main/java/com/guttery/madii/domain/joy/application/dto/JoyCreateResponse.java
+++ src/main/java/com/guttery/madii/domain/joy/application/dto/JoyCreateResponse.java
@@ -1,8 +1,6 @@
 package com.guttery.madii.domain.joy.application.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 @Schema(description = "소확행 기록 응답")
 public record JoyCreateResponse(
