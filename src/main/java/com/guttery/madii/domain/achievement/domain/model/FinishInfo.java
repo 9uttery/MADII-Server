ad6665a4fdb4bb51523e9c6787cc6f3e5a71ed18
mingeun0507 <tqdlfkg@naver.com>
2024-02-10 07:55:19 +0700

    feat: Achievement JPA 도메인 구현 (Entity, Repository, QueryDslRepository) (#85)

---
+++ src/main/java/com/guttery/madii/domain/achievement/domain/model/FinishInfo.java
@@ -0,0 +1,34 @@
+package com.guttery.madii.domain.achievement.domain.model;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Embeddable
+public class FinishInfo {
+    private Boolean isFinished;
+    private LocalDateTime finishedAt;
+    @Enumerated(EnumType.STRING)
+    private Satisfaction satisfaction;
+
+    public static FinishInfo createNotFinished() {
+        return new FinishInfo(false, null, null);
+    }
+
+    public static FinishInfo createFinished() {
+        return new FinishInfo(true, LocalDateTime.now(), null);
+    }
+
+    public static FinishInfo createdRated(Satisfaction satisfaction) {
+        return new FinishInfo(true, LocalDateTime.now(), satisfaction);
+    }
+}
---
+++ src/main/java/com/guttery/madii/domain/achievement/domain/repository/AchievementQueryDslRepository.java
@@ -0,0 +1,4 @@
+package com.guttery.madii.domain.achievement.domain.repository;
+
+public interface AchievementQueryDslRepository {
+}
