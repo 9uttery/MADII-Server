b084aa1754b3fcca605fa7f2a1930b640c7aa8dc
mingeun0507 <tqdlfkg@naver.com>
2024-02-11 10:16:26 +0700

    fix: 스케줄링 활성화 (#85)

---
+++ src/main/java/com/guttery/madii/common/config/scheduling/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.guttery.madii.common.config.scheduling;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
