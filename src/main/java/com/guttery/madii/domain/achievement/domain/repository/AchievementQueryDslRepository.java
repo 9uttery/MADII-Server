b88048e4f13cf033a217c3c19908aa5422aad1c4
mingeun0507 <tqdlfkg@naver.com>
2024-02-17 20:18:29 +0900

    feat: 실천 조회에 필요한 쿼리 구현 (#76)

--- src/main/java/com/guttery/madii/domain/achievement/domain/repository/AchievementQueryDslRepository.java
+++ src/main/java/com/guttery/madii/domain/achievement/domain/repository/AchievementQueryDslRepository.java
@@ -1,4 +1,9 @@
 package com.guttery.madii.domain.achievement.domain.repository;
 
+import com.guttery.madii.domain.achievement.application.dto.JoyPlaylistResponse;
+
+import java.time.LocalDate;
+
 public interface AchievementQueryDslRepository {
+    JoyPlaylistResponse getAchievementsInPlaylist(Long userId, LocalDate date);
 }
