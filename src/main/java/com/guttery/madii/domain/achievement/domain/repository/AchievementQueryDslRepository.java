699444675df62cd64a5f8b4350feab1e617f7b37
mingeun0507 <tqdlfkg@naver.com>
2024-02-17 22:32:59 +0900

    feat: 일자별 소확행 정보를 받아오는 서비스 및 쿼리 구현 (#77)

--- src/main/java/com/guttery/madii/domain/achievement/domain/repository/AchievementQueryDslRepository.java
+++ src/main/java/com/guttery/madii/domain/achievement/domain/repository/AchievementQueryDslRepository.java
@@ -1,9 +1,12 @@
 package com.guttery.madii.domain.achievement.domain.repository;
 
+import com.guttery.madii.domain.achievement.application.dto.CalenderAchievementColorResponse;
 import com.guttery.madii.domain.achievement.application.dto.JoyPlaylistResponse;
 
 import java.time.LocalDate;
 
 public interface AchievementQueryDslRepository {
     JoyPlaylistResponse getAchievementsInPlaylist(Long userId, LocalDate date);
+
+    CalenderAchievementColorResponse getAchievementsInCalender(Long userId, LocalDate startDate, LocalDate endDate);
 }
