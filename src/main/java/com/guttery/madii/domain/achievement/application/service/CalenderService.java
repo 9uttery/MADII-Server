640e1bc1bfa861b53f2333456633214feea164f7
mingeun0507 <tqdlfkg@naver.com>
2024-02-17 22:52:25 +0900

    feat: 캘린더 특정 기간 소확행 조회 쿼리 구현 (#77)

--- src/main/java/com/guttery/madii/domain/achievement/application/service/CalenderService.java
+++ src/main/java/com/guttery/madii/domain/achievement/application/service/CalenderService.java
@@ -1,6 +1,7 @@
 package com.guttery.madii.domain.achievement.application.service;
 
 import com.guttery.madii.domain.achievement.application.dto.CalenderAchievementColorResponse;
+import com.guttery.madii.domain.achievement.application.dto.CalenderDailyJoyAchievementResponse;
 import com.guttery.madii.domain.achievement.domain.repository.AchievementRepository;
 import com.guttery.madii.domain.joy.domain.repository.JoyRepository;
 import com.guttery.madii.domain.user.application.service.UserServiceHelper;
@@ -27,5 +28,12 @@ public class CalenderService {
         final User user = UserServiceHelper.findExistingUser(userRepository, userPrincipal);
 
         return achievementRepository.getAchievementsInCalender(user.getUserId(), startDate, endDate);
+    }
+
+    @Transactional(readOnly = true)
+    public CalenderDailyJoyAchievementResponse getDailyJoyAchievementInfos(final LocalDate date, final UserPrincipal userPrincipal) {
+        final User user = UserServiceHelper.findExistingUser(userRepository, userPrincipal);
+
+        return achievementRepository.getDailyJoyAchievementInfos(user.getUserId(), date);
     }
 }
--- src/main/java/com/guttery/madii/domain/achievement/domain/repository/AchievementQueryDslRepository.java
+++ src/main/java/com/guttery/madii/domain/achievement/domain/repository/AchievementQueryDslRepository.java
@@ -1,6 +1,7 @@
 package com.guttery.madii.domain.achievement.domain.repository;
 
 import com.guttery.madii.domain.achievement.application.dto.CalenderAchievementColorResponse;
+import com.guttery.madii.domain.achievement.application.dto.CalenderDailyJoyAchievementResponse;
 import com.guttery.madii.domain.achievement.application.dto.JoyPlaylistResponse;
 
 import java.time.LocalDate;
@@ -9,4 +10,6 @@ public interface AchievementQueryDslRepository {
     JoyPlaylistResponse getAchievementsInPlaylist(Long userId, LocalDate date);
 
     CalenderAchievementColorResponse getAchievementsInCalender(Long userId, LocalDate startDate, LocalDate endDate);
+
+    CalenderDailyJoyAchievementResponse getDailyJoyAchievementInfos(Long userId, LocalDate date);
 }
