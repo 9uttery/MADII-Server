3abff750ab96156ef1d73aa7d9cb0959a09e219f
mingeun0507 <tqdlfkg@naver.com>
2024-02-17 20:49:46 +0900

    docs: Swagger 및 Validation (#76)

--- src/main/java/com/guttery/madii/domain/achievement/application/dto/CancelAchievementRequest.java
+++ src/main/java/com/guttery/madii/domain/achievement/application/dto/CancelAchievementRequest.java
@@ -1,6 +1,12 @@
 package com.guttery.madii.domain.achievement.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+@Schema(description = "소확행 취소 요청")
 public record CancelAchievementRequest(
+        @NotNull
+        @Schema(description = "소확행 ID", example = "1")
         Long achievementId
 ) {
 }
