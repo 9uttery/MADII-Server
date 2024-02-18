f9189be8311cacb5286cc93e51a47253f3571edb
mingeun0507 <tqdlfkg@naver.com>
2024-02-18 13:45:08 +0900

    refactor: MongoDB 관련 부분 삭제 (#102)

--- src/main/java/com/guttery/madii/domain/achievement/domain/repository/UserPlaylistsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.guttery.madii.domain.achievement.domain.repository;
-
-import com.guttery.madii.domain.achievement.domain.model.UserPlaylists;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserPlaylistsRepository extends MongoRepository<UserPlaylists, Long>, UserPlaylistsMongoTemplateRepository {
-}
