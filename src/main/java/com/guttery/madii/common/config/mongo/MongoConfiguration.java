e3a138bbd9a3242479da4bad20808e8532aa0a61
mingeun0507 <tqdlfkg@naver.com>
2024-02-18 14:38:55 +0900

    fix: MongoDB 설정 잘못 되었던 버그 수정 (#102)

--- src/main/java/com/guttery/madii/common/config/mongo/MongoConfiguration.java
+++ src/main/java/com/guttery/madii/common/config/mongo/MongoConfiguration.java
@@ -2,12 +2,9 @@ package com.guttery.madii.common.config.mongo;
 
 import com.guttery.madii.common.converter.StringToYearMonthConverter;
 import com.guttery.madii.common.converter.YearMonthToStringConverter;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
 import java.util.Arrays;
@@ -18,16 +15,6 @@ public class MongoConfiguration {
 
     @Value("${spring.data.mongodb.uri}")
     private String mongoDbUrl;
-
-    @Bean
-    MongoClient mongoClient() {
-        return MongoClients.create(mongoDbUrl);
-    }
-
-    @Bean
-    MongoTemplate mongoTemplate(MongoClient mongoClient) {
-        return new MongoTemplate(mongoClient, MONGO_DB_NAME);
-    }
 
     @Bean
     public MongoCustomConversions mongoCustomConversions() {
