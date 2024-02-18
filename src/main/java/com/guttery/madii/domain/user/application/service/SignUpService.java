761301fac37b370059ba3488f3ba6ad720705505
mingeun0507 <tqdlfkg@naver.com>
2024-02-18 13:30:43 +0900

    fix: 로그인 응답 수정(#100)

--- src/main/java/com/guttery/madii/domain/user/application/service/SignUpService.java
+++ src/main/java/com/guttery/madii/domain/user/application/service/SignUpService.java
@@ -2,6 +2,8 @@ package com.guttery.madii.domain.user.application.service;
 
 import com.guttery.madii.common.exception.CustomException;
 import com.guttery.madii.common.exception.ErrorDetails;
+import com.guttery.madii.common.jwt.JwtProvider;
+import com.guttery.madii.domain.user.application.dto.LoginResponse;
 import com.guttery.madii.domain.user.application.dto.SignUpRequest;
 import com.guttery.madii.domain.user.domain.model.User;
 import com.guttery.madii.domain.user.domain.repository.UserRepository;
@@ -17,8 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(propagation = Propagation.REQUIRES_NEW)
 public class SignUpService {
     private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
 
-    public void signUp(final SignUpRequest signUpRequest) {
+    public LoginResponse signUp(final SignUpRequest signUpRequest) {
         if (isDuplicatedLoginId(signUpRequest.loginId())) {
             throw CustomException.of(ErrorDetails.DUPLICATED_LOGIN_ID);
         }
@@ -26,6 +29,8 @@ public class SignUpService {
         final User user = User.createNormalUser(signUpRequest.loginId(), signUpRequest.password(), signUpRequest.agreesMarketing());
 
         userRepository.save(user);
+
+        return new LoginResponse(jwtProvider.generateAccessToken(user.getUserId(), user.getRole()), jwtProvider.generateRefreshToken(user.getUserId(), user.getRole()), user.getAgreesMarketing(), user.hasProfile());
     }
 
     public boolean isDuplicatedLoginId(final String loginId) {
