d29c0de69d7b263393f72f5b20a5390ebf86d400
mingeun0507 <tqdlfkg@naver.com>
2024-02-18 16:23:11 +0900

    fix: 캘린더 API 응답 형식 수정 (#102)

---
+++ src/main/java/com/guttery/madii/domain/achievement/application/dto/AchievementColorInfo.java
@@ -0,0 +1,6 @@
+package com.guttery.madii.domain.achievement.application.dto;
+
+public record AchievementColorInfo(
+        Integer colorNum
+) {
+}
--- src/main/java/com/guttery/madii/domain/achievement/application/dto/DailyAchievementColorInfos.java
+++ src/main/java/com/guttery/madii/domain/achievement/application/dto/DailyAchievementColorInfos.java
@@ -1,14 +1,23 @@
 package com.guttery.madii.domain.achievement.application.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @Schema(description = "일일 소확행 색상 정보 리스트")
-public record DailyAchievementColorInfos(
-        @Schema(description = "날짜", example = "2021-10-01")
-        String date,
-        @Schema(description = "소확행 색상 정보 리스트")
-        List<Integer> achievementColorInfos
-) {
+public final class DailyAchievementColorInfos {
+    @Schema(description = "날짜", example = "2021-10-01")
+    private final String date;
+    @Schema(description = "소확행 색상 정보 리스트")
+    private final List<Integer> achievementColorInfos;
+
+    public DailyAchievementColorInfos(String date, List<AchievementColorInfo> achievementColorInfos) {
+        this.date = date;
+        this.achievementColorInfos = achievementColorInfos.stream()
+                .map(AchievementColorInfo::colorNum)
+                .limit(9)
+                .toList();
+    }
 }
