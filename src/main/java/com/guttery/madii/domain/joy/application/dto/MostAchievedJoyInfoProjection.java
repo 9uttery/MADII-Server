6061784ff4aaa9800060598d3e5e858333604601
mingeun0507 <tqdlfkg@naver.com>
2024-02-21 17:41:39 +0900

    fix: 많이 실천한 소확행 응답 형식 수정 (#128)

--- src/main/java/com/guttery/madii/domain/joy/application/dto/MostAchievedJoyInfoProjection.java
+++ src/main/java/com/guttery/madii/domain/joy/application/dto/MostAchievedJoyInfoProjection.java
@@ -1,11 +1,11 @@
 package com.guttery.madii.domain.joy.application.dto;
 
 public interface MostAchievedJoyInfoProjection {
+    Long getJoyId();
     Integer getJoyIconNum();
-
     String getContents();
 
+    Integer getIsCreatedByMe();
     Integer getAchieveCount();
-
     Integer getAchieveRank();
 }
