0f1e770313de5d3891c7a8230172606a14973c1f
mingeun0507 <tqdlfkg@naver.com>
2024-02-29 02:40:21 +0900

    feat: 전송, 다중전송 메소드 구현 (#132)

--- src/main/java/com/guttery/madii/domain/notification/domain/service/NotificationClient.java
+++ src/main/java/com/guttery/madii/domain/notification/domain/service/NotificationClient.java
@@ -1,4 +1,11 @@
 package com.guttery.madii.domain.notification.domain.service;
 
+import com.guttery.madii.domain.notification.application.dto.NotificationData;
+
+import java.util.List;
+
 public interface NotificationClient {
+    void sendNotification(NotificationData notificationData, String token);
+
+    void sendNotificationForAll(NotificationData notificationData, List<String> tokens);
 }
