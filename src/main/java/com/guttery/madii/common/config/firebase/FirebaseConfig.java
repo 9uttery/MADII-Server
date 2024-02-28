c6e17fdf9e7d882af0118a77bfcc4e7f155261e3
mingeun0507 <tqdlfkg@naver.com>
2024-02-29 02:38:35 +0900

    fix: Firebase Bean에 등록하도록 수정 (#132)

--- src/main/java/com/guttery/madii/common/config/firebase/FirebaseConfig.java
+++ src/main/java/com/guttery/madii/common/config/firebase/FirebaseConfig.java
@@ -3,42 +3,42 @@ package com.guttery.madii.common.config.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.guttery.madii.common.exception.CustomException;
 import com.guttery.madii.common.exception.ErrorDetails;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.InputStream;
 
 @Configuration
-public class FirebaseConfig implements InitializingBean {
+public class FirebaseConfig {
     @Value("${firebase.config}")
     private String firebaseKeyFilePath;
     @Value("${firebase.project-id}")
     private String firebaseProjectId;
 
-    @Override
-    public void afterPropertiesSet() {
-        initialize();
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
     }
 
-    private void initialize() {
-        try (
-                final InputStream serviceAccount = new ClassPathResource(firebaseKeyFilePath).getInputStream();
-        ) {
-            final FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setProjectId(firebaseProjectId)
-                    .build();
+    @Bean
+    public FirebaseApp firebaseApp(GoogleCredentials credentials) {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
 
+        return FirebaseApp.initializeApp(options);
+    }
 
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
+    @Bean
+    GoogleCredentials googleCredentials() {
+        try (final InputStream serviceAccount = new ClassPathResource(firebaseKeyFilePath).getInputStream();) {
+            return GoogleCredentials.fromStream(serviceAccount);
         } catch (final Exception e) {
-            e.printStackTrace();
             throw CustomException.of(ErrorDetails.FIREBASE_INTEGRATION_FAILED);
         }
     }
