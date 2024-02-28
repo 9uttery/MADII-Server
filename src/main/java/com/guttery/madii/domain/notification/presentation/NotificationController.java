8d73d1d0b1c3f7fde510dbe2e131501b0d2b1655
mingeun0507 <tqdlfkg@naver.com>
2024-02-29 02:41:48 +0900

    feat: 알림 전송 및 쿼리 API 구현 (#132)

--- src/main/java/com/guttery/madii/domain/notification/presentation/NotificationController.java
+++ src/main/java/com/guttery/madii/domain/notification/presentation/NotificationController.java
@@ -2,9 +2,13 @@ package com.guttery.madii.domain.notification.presentation;
 
 import com.guttery.madii.domain.notification.application.dto.NotificationListResponse;
 import com.guttery.madii.domain.notification.application.dto.SaveTokenRequest;
-import com.guttery.madii.domain.notification.application.service.NotificationService;
+import com.guttery.madii.domain.notification.application.service.NotificationQueryService;
+import com.guttery.madii.domain.notification.application.service.NotificationSendService;
 import com.guttery.madii.domain.notification.application.service.UserTokenService;
 import com.guttery.madii.domain.user.domain.model.UserPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -24,9 +28,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Notification", description = "Notification 관련 API입니다.")
 public class NotificationController {
     private final UserTokenService userTokenService;
-    private final NotificationService notificationService;
+    private final NotificationQueryService notificationQueryService;
+    private final NotificationSendService notificationSendService;
 
     @PostMapping("/token")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "FCM 토큰 저장 성공",
+                            useReturnTypeSchema = true
+                    )
+            }
+    )
+    @Operation(summary = "FCM 토큰 저장 API", description = "FCM 토큰을 저장하는 API입니다.")
     public void saveUserToken(
             @RequestBody @Valid SaveTokenRequest request,
             @NotNull @AuthenticationPrincipal UserPrincipal userPrincipal
@@ -35,9 +50,34 @@ public class NotificationController {
     }
 
     @GetMapping
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "알림 조회 성공",
+                            useReturnTypeSchema = true
+                    )
+            }
+    )
+    @Operation(summary = "알림 조회 API", description = "알림 조회 API입니다.")
     public NotificationListResponse getNotificationList(
             @NotNull @AuthenticationPrincipal UserPrincipal userPrincipal
     ) {
-        return notificationService.getNotificationList(userPrincipal);
+        return notificationQueryService.getNotificationList(userPrincipal);
+    }
+
+    @PostMapping("/test/today")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "오늘의 소확행 알림 발송 성공",
+                            useReturnTypeSchema = true
+                    )
+            }
+    )
+    @Operation(summary = "오늘의 소확행 알림 발송 테스트 API", description = "오늘의 소확행 알림 발송 테스트 API입니다.")
+    public void sendNotification() {
+        notificationSendService.sendAndSaveTodayJoyNotifications();
     }
 }
