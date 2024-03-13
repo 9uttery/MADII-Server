889f3a51427fea4f03fd549f6bd3f03423c14a98
mingeun0507 <tqdlfkg@naver.com>
2024-03-13 16:50:10 +0900

    fix: 알림 및 공지사항 날짜 형식 수정 (#147)

--- src/main/java/com/guttery/madii/domain/notification/application/mapper/NotificationMapper.java
+++ src/main/java/com/guttery/madii/domain/notification/application/mapper/NotificationMapper.java
@@ -5,15 +5,13 @@ import com.guttery.madii.domain.notification.domain.model.Notification;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.time.format.DateTimeFormatter;
-
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class NotificationMapper {
     public static NotificationInfo toNotificationInfo(Notification notification) {
         return new NotificationInfo(
                 notification.getTitle(),
                 notification.getContents(),
-                notification.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                notification.getCreatedAt()
         );
     }
 }
