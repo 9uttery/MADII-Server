dc73123c74775609f80686934a9219bb1fe17913
hojeong2747 <hojeong2747@gmail.com>
2024-03-23 16:32:13 +0900

    fix: 더미 데이터 쿼리 수정 (#159)

--- src/main/resources/insertSql/madii_tag.sql
+++ src/main/resources/insertSql/madii_tag.sql
@@ -6,12 +6,12 @@ LIMIT 0, 300
 
 -- Date: 2024-03-16 21:47
 */
-INSERT INTO t_tag (`tag_id`,`created_at`,`modified_at`,`contents`,`tag_type`) VALUES (1,now(),now(),'활기찬','WHEN');
-INSERT INTO t_tag (`tag_id`,`created_at`,`modified_at`,`contents`,`tag_type`) VALUES (2,now(),now(),'여유로운','WHEN');
-INSERT INTO t_tag (`tag_id`,`created_at`,`modified_at`,`contents`,`tag_type`) VALUES (3,now(),now(),'울적한','WHEN');
-INSERT INTO t_tag (`tag_id`,`created_at`,`modified_at`,`contents`,`tag_type`) VALUES (4,now(),now(),'나 혼자서','WHO');
-INSERT INTO t_tag (`tag_id`,`created_at`,`modified_at`,`contents`,`tag_type`) VALUES (5,now(),now(),'둘이서','WHO');
-INSERT INTO t_tag (`tag_id`,`created_at`,`modified_at`,`contents`,`tag_type`) VALUES (6,now(),now(),'다 함께','WHO');
-INSERT INTO t_tag (`tag_id`,`created_at`,`modified_at`,`contents`,`tag_type`) VALUES (7,now(),now(),'특별한 도전을 할 수 있는','WHICH');
-INSERT INTO t_tag (`tag_id`,`created_at`,`modified_at`,`contents`,`tag_type`) VALUES (8,now(),now(),'지금 바로 할 수 있는','WHICH');
-INSERT INTO t_tag (`tag_id`,`created_at`,`modified_at`,`contents`,`tag_type`) VALUES (9,now(),now(),'일상 속에서 할 수 있는','WHICH');
+INSERT INTO t_tag (`tag_id`,`contents`,`tag_type`) VALUES (1,'활기찬','WHEN');
+INSERT INTO t_tag (`tag_id`,`contents`,`tag_type`) VALUES (2,'여유로운','WHEN');
+INSERT INTO t_tag (`tag_id`,`contents`,`tag_type`) VALUES (3,'울적한','WHEN');
+INSERT INTO t_tag (`tag_id`,`contents`,`tag_type`) VALUES (4,'나 혼자서','WHO');
+INSERT INTO t_tag (`tag_id`,`contents`,`tag_type`) VALUES (5,'둘이서','WHO');
+INSERT INTO t_tag (`tag_id`,`contents`,`tag_type`) VALUES (6,'다 함께','WHO');
+INSERT INTO t_tag (`tag_id`,`contents`,`tag_type`) VALUES (7,'특별한 도전을 할 수 있는','WHICH');
+INSERT INTO t_tag (`tag_id`,`contents`,`tag_type`) VALUES (8,'지금 바로 할 수 있는','WHICH');
+INSERT INTO t_tag (`tag_id`,`contents`,`tag_type`) VALUES (9,'일상 속에서 할 수 있는','WHICH');
