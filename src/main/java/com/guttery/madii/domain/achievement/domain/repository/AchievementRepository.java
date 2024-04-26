0ad0f04100f9a8cda9a5b07d459be01bf5c08757
mingeun0507 <tqdlfkg@naver.com>
2024-04-26 22:05:54 +0900

    feat: 미완료 소확행이 오플리에 존재하는 경우 에러 메시지 출력하도록 수정 (#180)

--- src/main/java/com/guttery/madii/domain/achievement/domain/repository/AchievementRepository.java
+++ src/main/java/com/guttery/madii/domain/achievement/domain/repository/AchievementRepository.java
@@ -1,7 +1,12 @@
 package com.guttery.madii.domain.achievement.domain.repository;
 
 import com.guttery.madii.domain.achievement.domain.model.Achievement;
+import com.guttery.madii.domain.joy.domain.model.Joy;
+import com.guttery.madii.domain.user.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AchievementRepository extends JpaRepository<Achievement, Long>, AchievementQueryDslRepository {
+
+    boolean existsByJoyAndAchieverAndFinishInfo_IsFinishedFalse(Joy joy, User user);
+
 }
