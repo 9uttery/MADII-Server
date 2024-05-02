7591909efea6cef8b279ee5c568d7c338a34cb2a
mingeun0507 <tqdlfkg@naver.com>
2024-05-02 21:53:52 +0900

    fix: 탈퇴 오류 수정

--- src/main/java/com/guttery/madii/domain/joy/domain/model/Joy.java
+++ src/main/java/com/guttery/madii/domain/joy/domain/model/Joy.java
@@ -1,11 +1,28 @@
 package com.guttery.madii.domain.joy.domain.model;
 
 import com.guttery.madii.common.domain.model.BaseTimeEntity;
+import com.guttery.madii.domain.albums.domain.model.SavingJoy;
 import com.guttery.madii.domain.user.domain.model.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -24,6 +41,8 @@ public class Joy extends BaseTimeEntity {
     private Integer joyIconNum; // 소확행 썸네일 아이콘 번호
     @Column(length = 30)
     private String contents; // 소확행 내용
+    @OneToMany(mappedBy = "joy", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SavingJoy> savingJoys;
 
     public Joy(User user, JoyType joyType, Integer joyIconNum, String contents) {
         this.user = user;
