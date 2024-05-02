407fd3806f817df8b6f6fb6a632811f5d20561ba
mingeun0507 <tqdlfkg@naver.com>
2024-05-02 22:05:32 +0900

    fix: 탈퇴 오류 수정

--- src/main/java/com/guttery/madii/domain/albums/domain/model/SavingJoy.java
+++ src/main/java/com/guttery/madii/domain/albums/domain/model/SavingJoy.java
@@ -2,7 +2,16 @@ package com.guttery.madii.domain.albums.domain.model;
 
 import com.guttery.madii.common.domain.model.BaseTimeEntity;
 import com.guttery.madii.domain.joy.domain.model.Joy;
-import jakarta.persistence.*;
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +29,7 @@ public class SavingJoy extends BaseTimeEntity {
     @JoinColumn(name = "joy_id", nullable = false)
     private Joy joy; // 소확행 id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "album_id", nullable = false)
+    @JoinColumn(name = "album_id")
     private Album album; // 앨범 id
 
     public SavingJoy(Joy joy, Album album) {
