1b80b9277c54fd53e94834c4ed60f0b1f5604f66
mingeun0507 <tqdlfkg@naver.com>
2024-05-07 03:40:44 +0900

    fix: prod 환경이 아니면 스케줄러 작동하지 않도록 수정 (#181)

--- src/main/java/com/guttery/madii/common/config/scheduling/SchedulerConfig.java
+++ src/main/java/com/guttery/madii/common/config/scheduling/SchedulerConfig.java
@@ -1,9 +1,11 @@
 package com.guttery.madii.common.config.scheduling;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@Profile("prod")
 public class SchedulerConfig {
 }
